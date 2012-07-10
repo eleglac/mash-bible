8e45a7dcfccb736c2efaadf6f55d94e238d043f8
Alex Ponting <happy.ajp050789@gmail.com>
2012-07-10 16:23:05 -0700

    auto-title generation

--- src/mash_bible/views/util.clj
+++ src/mash_bible/views/util.clj
@@ -25,7 +25,7 @@
   {:one 1 :two 2 :three 3 :four 4 :five 5 :six 6 :seven 7 :eight 8 :nine 9 :ten 10 :eleven 11})
 
 (def root 
-  "/app")
+  "/home/eleglac/Code/mash-bible")
 
 (def summaries 
   (str root "/resources/public/summaries/"))
