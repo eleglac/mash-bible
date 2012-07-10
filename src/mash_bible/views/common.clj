0fd5ee4fd390478501ae32b4ddfb1025d14eeb1d
Alex Ponting <happy.ajp050789@gmail.com>
2012-07-10 15:59:26 -0700

    araghrahg

--- src/mash_bible/views/common.clj
+++ src/mash_bible/views/common.clj
@@ -1,11 +1,6 @@
 (ns mash-bible.views.common
-<<<<<<< HEAD
-
-  (require [mash-bible.views.util :as util]
-           [clojure.string        :as s])
-=======
-  (require [mash-bible.views.util :as util])
->>>>>>> parent of 5b6aca1... Formatting of sidebar links
+  (:require [mash-bible.views.util :as util]
+            [clojure.string        :as s])
 
   (:use [noir.core :only [defpartial]]
         [hiccup.page-helpers]))
@@ -24,14 +19,10 @@
   [:div#sidebar
     [:ul
       [:li [:a {:href "/"} "Home"]]
-<<<<<<< HEAD
       (map 
         (fn [lol] 
           [:li [:a {:href (str "/" (name lol))} "Season " (s/capitalize (name lol))]]) 
         (keys util/ssn-to-eplist))
-=======
-      (map (fn [lol] [:li [:a {:href (str "/" (name lol))} "Season " lol]]) (keys util/ssn-to-eplist))
->>>>>>> parent of 5b6aca1... Formatting of sidebar links
       [:li "Search"]]
     (ad)])
 
@@ -41,9 +32,9 @@
     (slurp)
     ((fn [ep]
        [:div#content 
-          ep
-          [:br] [:br]
-          [:a {:href (str "/" (name ssnum) "/" epnum "/transcript")} "Episode Transcript"]]))))
+         ep
+         [:br] [:br]
+         [:a {:href (str "/" (name ssnum) "/" epnum "/transcript")} "Episode Transcript"]]))))
 
 (defpartial transcript [ssnum epnum] 
   (->> (str util/transcripts (util/sym-to-num (keyword ssnum)) " x " epnum ".txt")
@@ -59,7 +50,7 @@
 
 (defpartial episode-list [ssn eps]
   [:div#content
-    [:h2 "Season " ssn]
+    [:h2 "Season " (s/capitalize (name ssn))]
     [:ul 
       (map 
         (fn [foo] 
