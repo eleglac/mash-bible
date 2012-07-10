4e3490172c3da4924e5ea1e972593e45c8b4f6be
Alex Ponting <happy.ajp050789@gmail.com>
2012-07-10 16:16:19 -0700

    unfucked NOW?

--- src/mash_bible/views/welcome.clj
+++ src/mash_bible/views/welcome.clj
@@ -15,11 +15,7 @@
     (common/footer)))
 
 (defpage [:get "/:ssnum"] {:keys [ssnum]}  
-<<<<<<< HEAD
   (common/layout (str "Season " (s/capitalize ssnum) " - Episode Listing - The M*A*S*H Bible")
-=======
-  (common/layout (str "The M*A*S*H Bible, the #1 resource for fans of televisions' M*A*S*H 4077th - Episode Listing")
->>>>>>> parent of 5b6aca1... Formatting of sidebar links
     (common/sidebar)
     (->>
       (keyword ssnum)
@@ -27,22 +23,14 @@
       (common/episode-list ssnum))
     (common/footer)))
 
-(defpage [:get "/:ssnum/:epnum"] {:keys [ssnum epnum]}  
-<<<<<<< HEAD
+(defpage [:get "/:ssnum/:epnum"] {:keys [ssnum epnum]}
   (common/layout (str (util/get-ep-title ssnum epnum) " - Summary - The M*A*S*H Bible")
-=======
-  (common/layout "The M*A*S*H Bible, the #1 resource for fans of the M*A*S*H 4077th - Episode Summary"
->>>>>>> parent of 5b6aca1... Formatting of sidebar links
     (common/sidebar)
     (common/cont ssnum epnum)
     (common/footer)))
 
 (defpage [:get "/:ssnum/:epnum/transcript"] {:keys [ssnum epnum]}
-<<<<<<< HEAD
   (common/layout (str (util/get-ep-title ssnum epnum) " - Transcript - The M*A*S*H Bible")
-=======
-  (common/layout "The M*A*S*H Bible, the #1 resource for fans of the M*A*S*H 4077th - Episode Transcript"
->>>>>>> parent of 5b6aca1... Formatting of sidebar links
     (common/sidebar)
     (common/transcript ssnum epnum)
     (common/footer)))
